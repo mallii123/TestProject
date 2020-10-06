58e87ed2d6247bc1f5ba9aff8701c543a6cea8b3
Patryk Malinowski <34032943+mallii123@users.noreply.github.com>
2020-10-06 14:43:37 +0200

    Refactoring
    Refactoring usings

--- CompanyData/Entities/User.cs
+++ CompanyData/Entities/User.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace CompanyData.Entities
+﻿namespace CompanyData.Entities
 {
     public class User
     {
